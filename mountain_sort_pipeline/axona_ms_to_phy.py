423feaba2001ff561f8fdc57f1fc31d36cb5ed7b
Predrag Živadinović <predrag.zivadinovic@ist.ac.at>
2022-02-15 13:59:42 +0100

    Fix writing params.json

--- mountain_sort_pipeline/axona_ms_to_phy.py
+++ mountain_sort_pipeline/axona_ms_to_phy.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 from os.path import join
 from os import scandir
-from pathlib import Path
+import json
 import spikeinterface as si
 from spikeinterface.extractors  import MdaRecordingExtractor, MdaSortingExtractor
 from spikeinterface.exporters import export_to_phy
@@ -11,12 +11,18 @@ import spikeinterface.widgets as sw
 from joblib import Parallel, delayed
 
 
-def __process_tet(tet_dir):
+def __write_params_json(path, **params):
+    print(params)
+    with open(path, "w") as pf:
+        json.dump(params, pf)
+
+
+def __process_tet(tet_dir, sampling_rate):
     print(f"Processing {tet_dir}")
-    Path(join(tet_dir, "params.json")).write_text(params_json)
+    __write_params_json(join(tet_dir, "params.json"), samplerate=sampling_rate)
     recording = MdaRecordingExtractor(tet_dir, raw_fname="raw_filt.mda")
     recording.annotate(is_filtered=True)
-    sorting = MdaSortingExtractor(join(tet_dir, "firings.mda"), args.sampling_rate)
+    sorting = MdaSortingExtractor(join(tet_dir, "firings.mda"), sampling_rate)
     wfe = WaveformExtractor.create(recording, sorting, join(tet_dir, "wfe"))
     wfe.set_params()
     wfe.run_extract_waveforms()
@@ -32,12 +38,11 @@ args.add_argument("--sampling_rate", type=int, default=24000, help="Sampling rat
 args.add_argument("--n_jobs", type=int, default=1, help="How many tetrodes to process in parallel (default is 1)")
 args = args.parse_args()
 
-params_json = '{ "samplerate": 24000 }'
 
 tet_dirs = [tet for tet in scandir(args.sorted_dir)]
 tet_dirs = list(filter(lambda tet: tet.is_dir() and tet.name.startswith("tet"), tet_dirs))
 
 if args.n_jobs <= 1:
-    [__process_tet(tet.path) for tet in tet_dirs]
+    [__process_tet(tet.path, args.sampling_rate) for tet in tet_dirs]
 else:
-    Parallel(n_jobs=args.n_jobs)(delayed(__process_tet)(tet.path) for tet in tet_dirs)
+    Parallel(n_jobs=args.n_jobs)(delayed(__process_tet, args.sampling_rate)(tet.path) for tet in tet_dirs)
